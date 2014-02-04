60d6a8d67479e6a37421e16edb16ce5e62f91dc6
Johan Brook <johan.jvb@gmail.com>
2014-02-04 22:57:20 +0100

    Add sample tests

--- test/tests.js
+++ test/tests.js
@@ -1,16 +1,65 @@
-var should = chai.should();
+var should = chai.should()
 
 describe("IndexedDBStore", function() {
 
 	var db = new IndexedDBStore({
 		dbName: "test"
-	});
+	})
 
 	beforeEach(function() {
-		db.clear();
-	});
+		db.clear()
+	})
+
+	it("should be available", function(){
+		db.should.not.be.undefined
+	})
 
 	it("should have a database name", function() {
-		db.name.should.equal("test");
-	});
-});
+		db.name.should.equal("test")
+	})
+
+	describe("#all()", function() {
+		it("should return no elements on default", function() {
+			return db.all().then(function(records) {
+				records.length.should.equal(0)
+			})
+		})
+
+		it("should return one element when added", function() {
+			return db.save("Test").then(function() {
+				return db.all().then(function(records) {
+					records.length.should.equal(1)
+				})
+			})
+		})
+	})
+
+	describe("#save()", function() {
+		it("should save a record", function() {
+			return db.save("Test").then(function(record) {
+				record.should.equal("Test")
+			})
+		})
+	})
+
+	describe("#get()", function() {
+		it("should retrieve a given record", function() {
+			return db.save("Test").then(function(record) {
+				record.should.equal("Test")
+
+				return db.get("Test").then(function(test) {
+					test.should.equal(record).and.equal("Test")
+				})
+			})
+		})
+	})
+
+	describe("#clear()", function() {
+		it("should clear all rows", function() {
+			db.clear()
+			return db.all().then(function(records) {
+				records.length.should.equal(0)
+			})
+		})
+	})
+})
